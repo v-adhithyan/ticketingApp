9d8c297a003635c850616f4f4f622fabe2c9a415
Adhithyan V <aavispeaks@gmail.com>
2018-03-26 12:17:53 +0530

    bluetooth printer code added

--- app/src/main/java/ceg/avtechlabs/standticket/MainActivity.kt
+++ app/src/main/java/ceg/avtechlabs/standticket/MainActivity.kt
@@ -1,6 +1,10 @@
 package ceg.avtechlabs.standticket
 
+import android.app.Activity
 import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
@@ -8,13 +12,18 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.os.Handler
+import android.os.Looper
 import android.support.v4.content.FileProvider
 import android.text.Spannable
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import ceg.avtechlabs.standticket.ui.CloseTicketActivity
+import ceg.avtechlabs.standticket.utils.PrinterCommands
+import ceg.avtechlabs.standticket.utils.Utils
 import ceg.avtechlabs.standticket.utils.generateQr
 import ceg.avtechlabs.standticket.utils.getDateTime
 import com.tom_roush.pdfbox.pdmodel.PDDocument
@@ -25,10 +34,23 @@ import com.tom_roush.pdfbox.pdmodel.font.PDType1Font
 import com.tom_roush.pdfbox.pdmodel.graphics.image.LosslessFactory
 import com.tom_roush.pdfbox.util.PDFBoxResourceLoader
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
+import java.io.*
+import java.nio.file.Paths
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val ENABLE_BLUETOOTH = 3592
+    var device: BluetoothDevice ? = null
+    var socket: BluetoothSocket? = null
+    var outStream: OutputStream? = null
+    var inStream: InputStream? = null
+
+    var workerThread: Thread? = null
+    var readBuffer: ByteArray? = null
+    var readBufferPosition = 0
+    var stopWorker = false
+    val value = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +64,10 @@ class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "Error occurred", Toast.LENGTH_LONG).show()
         }*/
+
+        //password: ticket, alias: ticket
+        //fname: adhi
+        enableBluetoothAndPrinterSetup()
     }
 
     fun setup() {
@@ -76,7 +102,7 @@ class MainActivity : AppCompatActivity() {
         progress.setCancelable(false)
         progress.show()
 
-        Thread {
+        /*Thread {
             val doc = PDDocument()
             val page = PDPage()
             doc.addPage(page)
@@ -159,7 +185,9 @@ class MainActivity : AppCompatActivity() {
                 doc.save(path)
                 doc.close()
 
-                runOnUiThread {
+                //var uri = ""
+                //runOnUiThread {
+                Looper.prepare()
                     Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
                     clear()
                     progress.dismiss()
@@ -168,23 +196,76 @@ class MainActivity : AppCompatActivity() {
 
                     val uri = FileProvider.getUriForFile(this,  this.applicationContext.packageName + ".provider",
                             File(path))
-                    intent.setDataAndType(uri, "application/pdf")
-                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                    startActivity(intent)
+
+                    val file = File(path)
+                    val fis = FileInputStream(file)
+                val bos = ByteArrayOutputStream()
+                val buf = ByteArray(1024)
+                val printFormat = ByteArray(3)
+                printFormat[0] = 27
+                printFormat[1] = 33
+                printFormat[2] = 0
+
+                try {
+                    //outStream?.write(printFormat)
+                    while (true) {
+                        val read = fis.read(buf)
+                        if(read == -1) {
+                            break
+                        }
+                        bos?.write(buf, 0, read)
+                    }
+                } catch (ex:Exception) {
+                    Toast.makeText(this@MainActivity, ex.message, Toast.LENGTH_LONG).show()
+
                 }
+                val bytes = bos.toByteArray()
+                outStream?.write(printFormat)
+                outStream?.write(bytes)
+                Toast.makeText(this@MainActivity, "printed", Toast.LENGTH_LONG).show()
+                /*intent.setDataAndType(uri, "application/pdf")
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivity(intent)*/
+                //}
+
 
 
             } catch (ex: Exception) {
                 ex.printStackTrace()
 
                 runOnUiThread {
-                    Toast.makeText(this, "Fail", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Fail ${ex.message}", Toast.LENGTH_LONG).show()
                     clear()
                     progress.dismiss()
                 }
             }
 
-        }.start()
+        }.start()*/
+
+        val standName = "POLLACHI MUNICIPALITY\nTWO WHEELER PARKING\n"
+        val address = "(Behind Nachimuthu nursing home)\n"
+        val tokenNo = "TOKEN: 123\n"
+        val vechicleNo = "Vehicle Number: ${editTextVehicleNo.text.toString()}\n"
+        val dateTime = "TIME: ${dateTimeView.text.toString()}\n"
+        val instructions = ""
+
+        val bitmap = Bitmap.createScaledBitmap(qrCode, (qrCode.width).toInt(), (qrCode.height).toInt(), true)
+        //bitmap.compress(Bitmap.CompressFormat.PNG, 100, qrCodeStream)
+
+        val header = "$standName$address"
+
+        val ticketString = "$tokenNo$vechicleNo$dateTime"
+
+        outStream?.write(PrinterCommands.ESC_ALIGN_CENTER)
+        outStream?.write(header.toByteArray())
+        outStream?.write(Utils.decodeBitmap(qrCode))
+        outStream?.write("\n".toByteArray())
+        outStream?.write(PrinterCommands.ESC_ALIGN_LEFT)
+        outStream?.write(ticketString.toByteArray())
+        outStream?.write(("\n\n\n" +
+                "").toByteArray())
+        clear()
+        progress.dismiss()
     }
 
     fun closeTicket(v: View) {
@@ -193,6 +274,133 @@ class MainActivity : AppCompatActivity() {
 
     fun clear() {
         editTextVehicleNo.setText("", TextView.BufferType.EDITABLE)
+
+    }
+
+    public val bluetoothAdapter by lazy {
+        BluetoothAdapter.getDefaultAdapter()
+    }
+
+    fun enableBluetoothAndPrinterSetup() {
+        try {
+            if(!bluetoothAdapter.isEnabled) {
+                val enableBluetooth = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBluetooth, ENABLE_BLUETOOTH)
+            } else {
+                connectPrinter()
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun connectPrinter() {
+        val pairedDevices = bluetoothAdapter.bondedDevices
+        for(d in pairedDevices) {
+            if(d.name.equals("BlueTooth Printer")) {
+                device = d
+                Toast.makeText(this, "Printer found", Toast.LENGTH_LONG).show()
+                break
+            }
+        }
+
+        val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        socket = device?.createRfcommSocketToServiceRecord(uuid)
+        socket?.connect()
+        outStream = socket?.outputStream
+        inStream = socket?.inputStream
+        Toast.makeText(this, "Stream opened", Toast.LENGTH_LONG).show()
+        beginListenForData()
+        try {
+            val msg = "Adhithyan Vijayakumar\n\n\n\n"
+            outStream?.write(msg.toByteArray())
+            Toast.makeText(this, "printed", Toast.LENGTH_LONG).show()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    fun print() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            ENABLE_BLUETOOTH -> {
+                if(resultCode == Activity.RESULT_OK) { connectPrinter()
+                } else {
+                    Toast.makeText(this, "Try again", Toast.LENGTH_LONG).show()
+                }
+            }   else -> {
+            Toast.makeText(this, "Turn on bluetooth to continue", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
+    internal fun beginListenForData() {
+        try {
+            val handler = Handler()
+
+            // this is the ASCII code for a newline character
+            val delimiter: Byte = 10
+
+            stopWorker = false
+            readBufferPosition = 0
+            readBuffer = ByteArray(1024)
+
+            workerThread = Thread(Runnable {
+                while (!Thread.currentThread().isInterrupted && !stopWorker) {
+
+                    try {
+
+                        val bytesAvailable = inStream!!.available()
+
+                        if (bytesAvailable > 0) {
+
+                            val packetBytes = ByteArray(bytesAvailable)
+                            inStream!!.read(packetBytes)
+
+                            for (i in 0 until bytesAvailable) {
+
+                                val b = packetBytes[i]
+                                if (b == delimiter) {
+
+                                    val encodedBytes = ByteArray(readBufferPosition)
+                                    System.arraycopy(
+                                            readBuffer, 0,
+                                            encodedBytes, 0,
+                                            encodedBytes.size
+                                    )
+
+                                    // specify US-ASCII encoding
+                                    val data = String(encodedBytes)
+                                    readBufferPosition = 0
+
+                                    // tell the user data were sent to bluetooth printer device
+                                    handler.post { Log.d("e", data) }
+
+                                } else {
+                                    readBuffer!![readBufferPosition++] = b
+                                }
+                            }
+                        }
+
+                    } catch (ex: IOException) {
+                        stopWorker = true
+                    }
+
+                }
+            })
+
+            workerThread?.start()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
     }
 }
